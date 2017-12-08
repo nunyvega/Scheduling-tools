590be914d38fa23c73c4134c2b1c17caac4557a3
nunyvega <nunyvega@hotmail.com>
2017-12-08 15:25:17 +0100

    Update schedganization.js

--- schedganization.js
+++ schedganization.js
@@ -1,19 +1,127 @@
 function myFunction() {
 }  
  
+
 /* Change name to userID */
+
 function ID(name) {
   var nameToId = name;
   switch(nameToId){
-    case "Juan":
-        return("IDEXAMPLE");
-        break;
-    case "juan":
-        return("ID EXAMPLE 2");
-        break;
-    default:
-        return("No ID Found");
-  }
+case "Aaron Portbury (PA)": return(3547614);
+case "Adeline Yaw (PA)": return(3547615);
+case "AI Sajib (R2)": return(3605118);
+case "Alicia Henry (DR)": return(3547599);
+case "Alvaro Vega (DR)": return(3547616);
+case "Amy Lane (ES)": return(3560330);
+case "Andrew dela Serna (PA)": return(3547603);
+case "Aniello Forlini (ES)": return(3547628);
+case "Anna Andrews-Mills (SP)": return(3562803);
+case "Anna McPhee (BE)": return(3605387);
+case "Asher Cantrell (AT)": return(3547583);
+case "Austin Gilmour (AT)": return(3548342);
+case "Ben Chilcote (HE)": return(3547648);
+case "Brezo Cordero (DR)": return(3562077);
+case "Bruce Allen (AT)": return(3547580);
+case "Carissa Faxlanger (AT)": return(3547578);
+case "Carla Doria Medina (TI)": return(3547566);
+case "Cat Scheer (BE)":return(3547637);
+case "Catia Kitahara (ES)": return(3547630);
+case "Charlie Coates (ES)": return(3544693);
+case "Chris Diehl (JU)": return(3704730);
+case "Chris Loder (AT)": return(3593423);
+case "Chris Rudzki (HE)": return(3543883);
+case "Cécile Rainon (LU)": return(3547643);
+case "Davi Pontes (AS)": return(3614800);
+case "David Cole (BE)": return(3546863);
+case "David Romero (DR)": return(3547595);
+case "Davor Altman (LU)": return(3603806);
+case "Dean (SP)": return(3547554);
+case "Denise Williams (CE)": return(3541324);
+case "Destiny Fox (BE)": return(3758719);
+case "Donal O'Conghaile (LU)": return(3620613);
+case "Drew Hackney (BE)": return(3546870);
+case "Ed Thomas (JU)": return(3762442);
+case "Eduardo Zulian (ES)": return(3547629);
+case "Ehtisham Siddiqui (LE)": return(3547619);
+case "Eric McDuffie (SP)": return(3547550);
+case "Gabriel Maldonado (DR)": return(3547596);
+case "Georgina Reeder (DR)": return(3547642);
+case "Gideon Koh (PA)": return(3592531);
+case "Glenn Pearson (SP)": return(3547549);
+case "Gracie Ofslager (HE)": return(3547645);
+case "Guga Alves (JU)": return(3543890);
+case "Guilherme Azeredo (TI)": return(3592905);
+case "Hafiz Rahman (LE)": return(3547620);
+case "Hari Shanker (LE)": return(3655008);
+case "Ian Markauskas (AS)": return(3711993);
+case "Irwin Chua (PA)": return(3547621);
+case "Ivan Blazevic (LU)": return(3596082);
+case "Jacklyn Lock (HE)": return(3547650);
+case "Jamie Palatnik (AS)": return(3650895);
+case "Jason Snow (SP)": return(3546864);
+case "Jay Mendoza (ES)": return(3547626);
+case "Jen McFadden (ES)": return(3543889);
+case "Jessica Gardner (HE)": return(3716409);
+case "Julia Sydnor (DR)": return(3547639);
+case "Kateryna Kodonenko (TI)": return(3547564);
+case "Keith Lock (TI)": return(3765953);
+case "Kevin Marsden (BE)": return(3546868);
+case "Khyati Gala (R2)": return(3592412);
+case "Kris Korn (LU)": return(3547640);
+case "Kristen Symonds (PA)": return(3550426);
+case "Kristin Snow (CE)": return(3543891);
+case "Kristina Daniele (HO)": return(3756473);
+case "Livio Vilela (AT)": return(3547586);
+case "Magdalena Olszewska (DR)": return(3559549);
+case "Marjorie Asturias (HE)": return(3547649);
+case "Mark Riley (JU)": return(3547571);
+case "Mark Senff (TI)": return(3547565);
+case "Maureen Carruthers (AT)": return(3547584);
+case "Megan Albritton (CE)": return(3547636);
+case "Melissa Silberstang (AS)": return(3594536);
+case "Mike Henry (LU)": return(3547644);
+case "Mike Straw (AS)": return(3611864);
+case "Mindy Postoff (ES)": return(3547632);
+case "Nagesh Pai (LE)": return(3547617);
+case "Niels Lange (LE)": return(3592532);
+case "Onyeka Amukpo (LU)": return(3756471);
+case "Pankaj Kumar (R2)": return(3623898);
+case "Rafael Funchal (ES)": return(3547631);
+case "Rahul Gupta (LE)": return(3543888);
+case "Raúl Antón Cuadrado (TI)": return(3547567);
+case "Richard Hunt (CE)": return(3593734);
+case "Richard Waynick (CE)": return(3546858);
+case "Rose Pajaroja (LE)": return(3756469);
+case "Rossana Menezes (JU)": return(3598707);
+case "Sajid Javed (R2)": return(3547594);
+case "Sam Hamdy (ES)": return(3547633);
+case "Sam Vaidya (HO)": return(3593733);
+case "Samantha Hare (ES)": return(3547627);
+case "Sarah Cada (PA)": return(3592413);
+case "Sarah Greenlee (AS)": return(3593975);
+case "Sarah Wolff Milligan (JU)": return(3547563);
+case "Sarah Woolsey (SP)": return(3547552);
+case "Shadae Trotman (AS)": return(3598669);
+case "Shane Tabrizi (CE)": return(3691862);
+case "Siobhan Bamber (LU)": return(3546866);
+case "Spencer Berry (HO)": return(3564749);
+case "Stephen Cosgrove (HO)": return(3598824);
+case "Steve Dixon (HE)": return(3716424);
+case "Steve Kryworuka (HE)": return(3547646);
+case "Sudipto Mondal (LE)": return(3548688);
+case "Suyogya Shukla (R2)": return(3592414);
+case "Synora Eusebio (AT)": return(3694222);
+case "Tiffany Miller (HO)": return(3623879);
+case "Tiffany Villa (BE)": return(3758718);
+case "Tish Briseno (SP)": return(3547555);
+case "Tony Arcangelini (HO)": return(3756474);
+case "Travis Dunne (CE)": return(3547635);
+case "Trevor Montgomery (AT)": return(3547582);
+case "Wyn Barnett (AT)": return(3593407);
+case "Yuki Hachiya (LE)": return(3592419);
+case "Zohair Yousafi (R2)": return(3730788);
+default: return("No ID Found");
+}
 };
 
 
@@ -61,7 +169,7 @@ function CSS(pref){
   else if(pref==="Live Chat"){
     prefResult="font-color:blue;";
   }
-    else if(pref==="No particular preferenc"){
+    else if(pref==="No particular preference"){
     prefResult="font-color:blue;";
   }
   else{
@@ -131,4 +239,58 @@ function fourHoursBlock(reply){
   return false;
   }
   return fourHoursBlockResult;
+}
+
+
+/* Business 1:1 */
+
+function business(reply){
+  var businessResult;
+  if (reply==="Full time"){
+    businessResult="color:red;";
+  }
+    else if(reply==="Part time"){
+    businessResult="color: green;";
+  }
+  else if(reply==="No"){
+    businessResult="color: blue;";
+  }
+  else {
+  return false;
+  }
+  return businessResult;
+}
+
+
+/* Team Lead */
+
+function teamLead(reply){
+  var teamLeadResult;
+  if (reply==="Yes"){
+    teamLeadResult="color:red;";
+  }
+  else if(reply==="No"){
+    teamLeadResult="color: blue;";
+  }
+  else {
+  return false;
+  }
+  return teamLeadResult;
+}
+
+
+/* Pressable Ambassador */
+
+function pressableAmbassador(reply){
+  var paResult;
+  if (reply==="Yes"){
+    paResult="color:red;";
+  }
+  else if(reply==="No"){
+    paResult="color: blue;";
+  }
+  else {
+  return false;
+  }
+  return paResult;
 }
