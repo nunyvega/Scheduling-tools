bea2a7408d821214a8ac4effb57c5a13e4875f12
nunyvega <nunyvega@hotmail.com>
2018-04-03 19:10:17 +0200

    Minor updates

--- Tampermonkey Scripts/Add preferences CSS.js
+++ Tampermonkey Scripts/Add preferences CSS.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Push CSS Humanity
 // @namespace    https://github.com/nunyvega/Scheduling-tools/
-// @version      5
+// @version      6
 // @description  Include HEs preferences in Humanity
 // @author       Alvaro Vega
 // @match        https://automattic.humanity.com/*
@@ -24,14 +24,14 @@
 (function() {
     'use strict';
 
-    var CSSMonday;
-    var CSSTuesday;
-    var CSSWednesday;
-    var CSSThursday;
-    var CSSFriday;
-    var CSSSaturday;
-    var CSSSunday;
-    var CSSWeek;
+    var CSSMonday= ' ';
+    var CSSTuesday= ' ';
+    var CSSWednesday= ' ';
+    var CSSThursday= ' ';
+    var CSSFriday= ' ';
+    var CSSSaturday= ' ';
+    var CSSSunday= ' ';
+    var CSSWeek = ' ';
     var textForDiv = [];
 
 
@@ -42,12 +42,15 @@
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CSSWeek +=`/*Alvaro Vega (DR)*/#e_3547566::after{ display:inline-block;content:'    MaxBiz 1';}#e_3547566{   ;}`;CSSMonday +=`/*Alvaro Vega (DR)*/  `;CSSTuesday +=`/*Alvaro Vega (DR)*/  `;CSSWednesday +=`/*Alvaro Vega (DR)*/  `;CSSThursday +=`/*Alvaro Vega (DR)*/  `;CSSFriday +=`/*Alvaro Vega (DR)*/  `;CSSSaturday +=`/*Alvaro Vega (DR)*/  `;CSSSunday+=`/*Alvaro Vega (DR)*/  `;textForDiv.push("1 biz 1:1 a week^3547566");
-CSSWeek +=`/*Alvaro Vega (DR)*/#e_3547637::after{ display:inline-block;content:'     ';}#e_3547637{   ;}`;CSSMonday +=`/*Alvaro Vega (DR)*/#d_3547637.day.dcol15 {background: rgba(253, 126, 20, 0.2)!important;} `;CSSTuesday +=`/*Alvaro Vega (DR)*/#d_3547637.day.dcol15 {background: rgba(253, 126, 20, 0.2)!important;} `;CSSWednesday +=`/*Alvaro Vega (DR)*/#d_3547637.day.dcol15 {background: rgba(253, 126, 20, 0.2)!important;} `;CSSThursday +=`/*Alvaro Vega (DR)*/#d_3547637.day.dcol15 {background: rgba(253, 126, 20, 0.2)!important;} `;CSSFriday +=`/*Alvaro Vega (DR)*/#d_3547637.day.dcol15 {background: rgba(253, 126, 20, 0.2)!important;} `;CSSSaturday +=`/*Alvaro Vega (DR)*/  `;CSSSunday+=`/*Alvaro Vega (DR)*/  `;
-CSSWeek +=`/*Alvaro Vega (DR)*/#e_3547630::after{ display:inline-block;content:'     ';}#e_3547630{   ;}`;CSSMonday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSTuesday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSWednesday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSThursday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSFriday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSSaturday +=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;CSSSunday+=`/*Alvaro Vega (DR)*/ #d_3547630.day.dcol8 ,#d_3547630.day.dcol9 ,#d_3547630.day.dcol10 ,#d_3547630.day.dcol11 {background: rgba(190,75,219,.1);}`;
+CSSWeek +=`/*Alvaro Vega (ORI)*/#e_3547598::after{ display:inline-block;content:'6hMax 3hRow 1hX 4hX ';}#e_3547598{background-color: rgba(253, 126, 20, 0.13) !important;;}#e_3547598 .udrag{background-color:#f9efff !important;}.dcol16#d_3547598{border: 5px solid #4a9abd !important;}`;textForDiv.push("if possible, chat at 5-7 UTC^3547598");
+CSSWeek +=`/*Alvaro Vega (HO)*/#e_3827916::after{ display:inline-block;content:'6hMax 2HRow 1hX 4h✓ ';}#e_3827916{background-color:rgba(190, 92, 251,0.1) !important;;}#e_3827916 `;
+CSSWeek +=`/*Alvaro Vega (ES)*/#e_3547627::after{ display:inline-block;content:'3hMax 3hRow 1hX 4hX MaxBiz 3❚❚✓';}#e_3547627{background-color: rgba(253, 126, 20, 0.13) !important;border-right: 5px dashed #5e3cc4 !important;;}#e_3547627 .udrag{background-color:#f9efff !important;}.dcol05#d_3547627{border: 5px solid #4a9abd !important;}`;textForDiv.push("*CHÉ, RE BIEN LA FORM^3547627");
+CSSWeek +=`/*Alvaro Vega (SP)*/#e_3547555::after{ display:inline-block;content:'2hMax 2HRow 1hX 4hX MaxBiz 10❚❚✓';}#e_3547555{background-color: #1068a72e !important;border-right: 5px dashed #5e3cc4 !important;border-left: 10px solid #6cd6fc !important;;}#e_3547555 .udrag{background-color:#f9efff !important;}.dcol07#d_3547555{border: 5px solid #4a9abd !important;}`;
+
+
+
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 
     // Step three:
     // Functions to detect the day, and insert a CSS code acording to that
@@ -60,7 +63,7 @@ CSSWeek +=`/*Alvaro Vega (DR)*/#e_3547630::after{ display:inline-block;content:'
         if (activeView2 === "employeeMode"){
             var node = document.createElement('style');
             // Add CSS rule to hide DIVs added with preferences under the names and show a message icon. When the mouse is over this icon, it will show a box with the preferences.
-            node.innerHTML = str + ".popup-parent {right:0px;top:0px;position:absolute;width:40px;height:30px;background-image:url('https://ru.reneelab.com/wp-content/uploads/sites/13/2017/02/laba.png');background-position:center;background-size:65%;background-repeat:no-repeat;z-index:999;}.popup-parent:hover {top:0;bottom:0;left:0;right:0;font-size:14px !important;background-color:white;text-align:justify;width:100%;height:100%;padding:10px;}.popup {background-color: white;border: solid 1px #c0c0c0;padding: 5px;display: none;}.popup-parent:hover .popup {display: block;}";
+            node.innerHTML =  str + ".popup-parent2 {right:0px;top:0px;position:absolute;width:40px;height:30px;background-image:url('http://icons.iconarchive.com/icons/elegantthemes/beautiful-flat/128/megaphone-2-icon.png');background-position:center;background-size:65%;background-repeat:no-repeat;z-index:999;} .popup-parent {right:0px;top:0px;position:absolute;width:40px;height:30px;background-image:url('https://ru.reneelab.com/wp-content/uploads/sites/13/2017/02/laba.png');background-position:center;background-size:65%;background-repeat:no-repeat;z-index:999;}.popup-parent:hover, .popup-parent2:hover {top:0;bottom:0;left:0;right:0;font-size:14px !important;background-color:white;text-align:justify;width:100%;height:100%;padding:10px;}.popup {background-color: white;border: solid 1px #c0c0c0;padding: 5px;display: none;}.popup-parent:hover .popup {display: block;} .popup-parent2:hover .popup{display:block;}";
             node.id = "customCSS";
             document.body.appendChild(node);
         }}
@@ -79,7 +82,8 @@ CSSWeek +=`/*Alvaro Vega (DR)*/#e_3547630::after{ display:inline-block;content:'
                 var idForDiv = "e_"+dividedContent[1];
                 var description = document.createElement('div');
                 var parentDiv = document.createElement('div');
-                parentDiv.className = "popup-parent";
+                if (requirementText[0] === "*"){parentDiv.className = "popup-parent2";}
+               else { parentDiv.className = "popup-parent"; }
                 parentDiv.id = "popup-parent"+idForDiv;
                 description.innerHTML = requirementText;
                 description.id = "popup"+ idForDiv;
