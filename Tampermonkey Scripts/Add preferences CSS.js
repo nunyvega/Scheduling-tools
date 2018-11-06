96d6c8b3cfade05bb8b4688cd8a4923d326e7a76
nunyvega <nunyvega@hotmail.com>
2018-11-06 11:53:08 +0100

    last weekly update

--- Tampermonkey Scripts/Add preferences CSS.js
+++ Tampermonkey Scripts/Add preferences CSS.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Push CSS Humanity
 // @namespace    https://github.com/nunyvega/Scheduling-tools/
-// @version      14.4
+// @version      15
 // @description  Include HEs preferences in Humanity
 // @author       Alvaro Vega
 // @match        https://automattic.humanity.com/*
